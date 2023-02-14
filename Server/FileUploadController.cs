2311cec1fd0052cd29e60bfc4b8c38eedcf8e936
Cstratton-brown <98555569+Cstratton-brown@users.noreply.github.com>
2023-02-14 17:43:21 +0000

    Add: Uploaded Server Files for Mobile App

---
+++ Server/FileUploadController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FileUploadController : ControllerBase
+{
+
+    private readonly IWebHostEnvironment env;
+
+    public FileUploadController
+(IWebHostEnvironment env)
+    {
+        this.env = env;
+    }
+
+    [HttpPost]
+    public void Post(UploadedFile uploadedFile)
+    {
+        var path = $"{env.WebRootPath}\\{uploadedFile.FileName}";
+        var fs = System.IO.File.Create(path);
+        fs.Write(uploadedFile.FileContent, 0,
+uploadedFile.FileContent.Length);
+        fs.Close();
+    }
+}
