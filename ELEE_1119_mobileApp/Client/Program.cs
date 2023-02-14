db5a81fdd0f2ed8c7dffd77de3ba8b29f46249a4
Cstratton-Brown <charlesstrattonbrown@gmail.com>
2023-02-14 17:57:43 +0000

    Add: Add all Client files

---
+++ ELEE_1119_mobileApp/Client/Program.cs
@@ -0,0 +1,14 @@
+using ELEE_1119_mobileApp.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("ELEE_1119_mobileApp.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ELEE_1119_mobileApp.ServerAPI"));
+
+await builder.Build().RunAsync();
